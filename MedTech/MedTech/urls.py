afb6430e116f11a3f473d47819f76fefbf6ffd86
Tanmay Kalani <tanmay.k16@iiits.in>
2018-09-22 00:17:35 +0530

    Basic UI

--- MedTech/MedTech/urls.py
+++ MedTech/MedTech/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('MedTechUser.urls', namespace='MedTechUser'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
