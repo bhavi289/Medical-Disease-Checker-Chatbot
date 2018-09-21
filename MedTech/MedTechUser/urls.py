5b4680ec091844f1e2232e7749bb8cfb958e8b44
Tanmay Kalani <tanmay.k16@iiits.in>
2018-09-21 20:41:45 +0530

    Initial

---
+++ MedTech/MedTechUser/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from . import views
+
+app_name = 'MedTechUser'
+
+urlpatterns = [
+    url(r'^$', views.home, name="home")
+]
